8bffd31b9f5412440b6bed1e6441d004d0b2e70f
Koray KOKSAL <tkkoksal@icloud.com>
2023-07-05 21:08:08 +0300

    settt

--- src/1-Events/Events.jsx
+++ src/1-Events/Events.jsx
@@ -2,15 +2,41 @@
 
 export const Events = () => {
 
-    const firstFunc=()=>{
+
+    let message = "Events"
+
+    const firstFunc=(e)=>{
 
         alert("Hello React")
+
+        console.log(e);
+
+
+    }
+
+    const handleChange=(e)=>{
+
+        console.log(e.target.name);
+        message = "State"
+
     }
 
   return (
     <div>
 
+        <h1>{message}</h1>
+
         <button onClick={firstFunc}>Click Me</button>
+
+        {/* Eger bir event fonksiyonunun paremetresi olmasi gerekiyorsa
+      bu fonksiyon bir arrow fonks. tarafindan  cagrilmalidir. Aksi
+      takdirde event fonksiyonu event gerceklesmeden cagirilir */}
+        <button onClick={() => alert("Deneme")}>Save</button>
+
+
+      <button onClick={handleChange}>Change</button>
+
+
 
     </div>
   )
