b97546b9d6231b26db764c4cab609589ecb8fb58
Koray KOKSAL <tkkoksal@icloud.com>
2023-07-05 20:27:06 +0300

    first commit

---
+++ src/1-Events/Events.jsx
@@ -0,0 +1,17 @@
+
+
+export const Events = () => {
+
+    const firstFunc=()=>{
+
+        alert("Hello React")
+    }
+
+  return (
+    <div>
+
+        <button onClick={firstFunc}>Click Me</button>
+
+    </div>
+  )
+}
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Events } from "./1-Events/Events"
+
+function App() {
+  return (
+    <div>
+      
+      <Events/>
+
+    </div>
+  )
+}
+export default App
